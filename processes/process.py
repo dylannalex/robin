27230614514113cbdc1408b617042899c2d958e4
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-10-31 21:49:16 -0300

    Add find_first_processes and update get_new_processes
    find_first_processes: finds all the processes that arrive first.
    get_new_processes: now returns the slept_processes list updated.

--- processes/process.py
+++ processes/process.py
@@ -24,12 +24,15 @@ def sort_processes_by_arrival_time(processes: list[Process]):
     return sorted(processes, key=lambda p: p.arrival_time)
 
 
-def get_new_processes(time, processes: list[Process]):
+def get_new_processes(time, slept_processes: list[Process]):
     """
     Returns a list of the processes which arrival_time is the
-    same as the given time
+    same as the given time and updates the slept_processes list
+    by removing the new processes from it
     """
-    return [p for p in processes if p.arrival_time == time]
+    new_processes = [p for p in slept_processes if p.arrival_time == time]
+    slept_processes = [p for p in slept_processes if p not in new_processes]
+    return new_processes, slept_processes
 
 
 def get_processes_names(processes: list[Process]):
@@ -41,3 +44,18 @@ def swap_processes(processes: list[Process], i, j):
     swaps the process at index i with the process at index j
     """
     processes[i], processes[j] = processes[j], processes[i]
+
+
+def find_first_processes(processes: list[Process]):
+    """
+    Finds all the processes that arrive first
+    """
+    sorted_proccesses = sort_processes_by_arrival_time(processes)
+    first_arrival_time = sorted_proccesses[0].arrival_time
+    first_processes = []
+    for process in sorted_proccesses:
+        if process.arrival_time != first_arrival_time:
+            return first_processes
+        first_processes.append(process)
+
+    return first_processes
