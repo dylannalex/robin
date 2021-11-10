b01f69e09319c4d71cfe35ebd52d65f56a751348
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-11-09 23:52:58 -0300

    Create units_converter.py

---
+++ input_output/units_converter.py
@@ -0,0 +1,55 @@
+def convert_size_unit(n, size_unit):
+    """
+    Converts given size unit to bytes
+    """
+    scale_factor = 1
+
+    if size_unit == "GB":
+        scale_factor = 1024 ** 3
+
+    elif size_unit == "MB":
+        scale_factor = 1024 ** 2
+
+    elif size_unit == "KB":
+        scale_factor = 1024
+
+    elif size_unit == "b":
+        scale_factor = 1 / 8
+
+    return n * scale_factor
+
+
+def convert_time_unit(n, time_unit):
+    """
+    Converts given time unit to ms
+    """
+    scale_factor = 1
+
+    if time_unit == "s":
+        scale_factor = 1000
+
+    if time_unit == "hour" or time_unit == "h":
+        scale_factor = 60 * 1000
+
+    return n * scale_factor
+
+
+def convert_transfer_velocity(n, size_unit, time_unit):
+    """
+    Converts velocity to bytes/ms
+    """
+
+    scale_factor = 1
+
+    if time_unit == "s":
+        scale_factor /= 1000
+
+    return convert_size_unit(n, size_unit) * scale_factor
+
+
+def decompose_number(n):
+    number = "".join([d for d in str(n) if d.isdigit()]).strip()
+    unit = "".join([u for u in str(n) if u not in number]).strip()
+    if not unit:
+        unit = "B"
+    return int(number), unit
