6da345ae8a33f6e55ed2c4548ed33091aeceb4dd
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-03-10 12:14:36 -0300

    Create __main__.py

---
+++ os_utn/tgm/__main__.py
@@ -0,0 +1,42 @@
+import telegram
+import telegram.ext
+from os_utn.tgm import settings
+from os_utn.tgm.commands import guide
+from os_utn.tgm.commands import parser
+from os_utn.tgm.callback import Callback
+
+
+if settings.MODE == "test":
+    # Local access
+    def run(updater):
+        updater.start_polling()
+        print("[STATUS] bot loaded")
+        updater.idle()  # End bot with ctrl + C
+
+elif settings.MODE == "deploy":
+    # Heroku access
+    def run(updater):
+        updater.start_webhook(
+            listen="0.0.0.0",
+            port=settings.PORT,
+            url_path=settings.TOKEN,
+            webhook_url=f"https://{settings.HEROKU_APP_NAME}.herokuapp.com/{settings.TOKEN}",
+        )
+
+
+def main() -> None:
+    bot = telegram.Bot(token=settings.TOKEN)
+    updater = telegram.ext.Updater(bot.token, use_context=True)
+    dp = updater.dispatcher
+
+    # Commands
+    dp.add_handler(telegram.ext.CommandHandler("start", guide.Guide.start))
+    dp.add_handler(
+        telegram.ext.MessageHandler(telegram.ext.Filters.text, parser.parser)
+    )
+    dp.add_handler(telegram.ext.CallbackQueryHandler(Callback.query_handler))
+    run(updater)
+
+
+if __name__ == "__main__":
+    main()
