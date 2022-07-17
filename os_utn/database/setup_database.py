e127d8c39c0b96e04358229d6742c9b06d18d911
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-07-17 14:07:56 -0300

    Create ```setup_database.py```

---
+++ os_utn/database/setup_database.py
@@ -0,0 +1,18 @@
+import os
+from os_utn.database import database
+from mysql.connector.connection_cext import CMySQLConnection
+
+
+def setup_tables(db: CMySQLConnection):
+    script_dir = os.path.dirname(__file__)
+    database.execute_script(db, os.path.join(script_dir, "scripts/create_tables.sql"))
+    database.execute_script(db, os.path.join(script_dir, "scripts/setup_tasks.sql"))
+
+
+def setup_database():
+    db = database.connect()
+    setup_tables(db)
+
+
+if __name__ == "__main__":
+    setup_database()
